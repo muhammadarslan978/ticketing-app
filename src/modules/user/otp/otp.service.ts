c5aa4879bbdc4d42864b3e678b8cd046a8ac84bc
muhammadarslan978 <arslan@99technologies.com>
2024-04-29 00:16:42 +0500

    added otp function and set http exception error

--- src/modules/user/otp/otp.service.ts
+++ src/modules/user/otp/otp.service.ts
@@ -2,8 +2,30 @@ import { Inject, Injectable } from '@nestjs/common'
 import { REPOSITORY } from 'src/constant'
 import { Otp } from 'src/modules/database/entity/otp'
 import { Repository } from 'typeorm'
+import * as moment from 'moment'
 
 @Injectable()
 export class OtpService {
-    constructor(@Inject(REPOSITORY.USER_REPOSITORY) private otpRepo: Repository<Otp>) {}
+    constructor(@Inject(REPOSITORY.OTP_REPOSITORY) private otpRepo: Repository<Otp>) {}
+
+    generateRandomSixDigitNumber(): number {
+        console.log('otp code', Math.floor(100000 + Math.random() * 900000))
+        return Math.floor(100000 + Math.random() * 900000)
+    }
+
+    async generateOtp(userId: string): Promise<any> {
+        try {
+            const now = moment()
+            const oneHourLatter = now.add('1', 'hours')
+            const otp = new Otp()
+            Object.assign(otp, {
+                userId: userId,
+                otpCode: this.generateRandomSixDigitNumber,
+                expirationTime: oneHourLatter,
+            })
+            await this.otpRepo.save(otp)
+        } catch (err) {
+            throw new Error(err.message)
+        }
+    }
 }
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Inject, Injectable } from '@nestjs/common'
+import { ConflictException, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
 import * as bcrypt from 'bcryptjs'
 
 import { CreateUserDto } from '../dto/user.dto'
@@ -6,25 +6,41 @@ import { promises } from 'dns'
 import { REPOSITORY } from 'src/constant'
 import { Repository } from 'typeorm'
 import { User } from 'src/modules/database/entity/user'
+import { OtpService } from '../otp/otp.service'
 
 @Injectable()
 export class UserService {
-    constructor(@Inject(REPOSITORY.USER_REPOSITORY) private userRepo: Repository<User>) {}
-
+    constructor(
+        @Inject(REPOSITORY.USER_REPOSITORY) private userRepo: Repository<User>,
+        private readonly otpService: OtpService,
+    ) {}
     async createUser(obj: CreateUserDto): Promise<any> {
         try {
-            const existedUser = await this.userRepo.findOne({ where: [{ email: obj.email }, { username: obj.username }] })
+            const existedUser = await this.userRepo.findOne({
+                where: [{ email: obj.email }, { username: obj.username }],
+            })
+
             if (existedUser) {
-                throw new ConflictException('User Already exist')
+                throw new HttpException('User already exists', HttpStatus.CONFLICT)
             }
+
             const hash = await bcrypt.hash(obj.password, 10)
             obj.password = hash
+
             const user = new User()
             Object.assign(user, obj)
-            const savedUser = this.userRepo.save(user)
+            const savedUser = await this.userRepo.save(user)
+
+            this.otpService.generateOtp(savedUser.id)
             return savedUser
         } catch (err) {
-            throw new Error(err.message)
+            // If it's an HTTP exception, rethrow it directly.
+            if (err instanceof HttpException) {
+                throw err
+            }
+            // Log and throw a meaningful HTTP exception instead of a generic Error.
+            console.error(err) // Log the error for debugging purposes.
+            throw new HttpException('Failed to create user due to internal server error', HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 
