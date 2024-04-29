19319ae1abe7c0f961e668273e5bcdbc6ba02334
muhammadarslan978 <arslan@99technologies.com>
2024-04-30 00:54:20 +0500

    signin api is added and fixed issues

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './controller/user.controller'
 import { UserService } from './service/user.service'
 import { DatabaseModule } from '../database/database.module'
 import { OtpService } from './otp/otp.service'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [UserController],
     providers: [UserService, OtpService],
 })
